328b006320d6482397a451edb8b75a92eb7b2935
Animal42069 <animalkdr@gmail.com>
2022-02-17 17:32:58 -0600

    Version 1.3.0 Release,  Apllying weather effects on lighting to shadow strength

--- AI_EnvironmentalLighting/AIEnvironmentalLighting.cs
+++ AI_EnvironmentalLighting/AIEnvironmentalLighting.cs
@@ -13,7 +13,7 @@ namespace EnvironmentalLighting
     [BepInProcess("AI-Syoujyo")]
     public class AIEnvironmentalLighting : BaseUnityPlugin
     {
-        public const string VERSION = "1.2.0.0";
+        public const string VERSION = "1.3.0.0";
         internal const string GUID = "animal42069.aienvironmentlighting";
         internal static Harmony harmony;
         internal static ConfigEntry<bool> _dhh_override;
@@ -69,39 +69,39 @@ namespace EnvironmentalLighting
         internal void Awake()
         {
             _dhh_override = Config.Bind("Graphics DHH", "Override", true, "Replace DHH's static light intensity values with the original dynamic environmental lighting");
-            _sun_intensity_multipier = Config.Bind("Sunlight", "Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply sun direct light intensity by before applying.", new AcceptableValueRange<float>(0, 1)));
-            _sun_intensity_offset = Config.Bind("Sunlight", "Intensity Offset", 0.0f, "Amount to offset sun direct light intensity by before applying.");
-            _sun_shadow_multipier = Config.Bind("Sunlight", "Shadow Strength Multiplier", 1.0f, new ConfigDescription("Amount to multiply sun direct light shadow strength by before applying.", new AcceptableValueRange<float>(0, 1)));
-            _moon_intensity_multipier = Config.Bind("Moonlight", "Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply moon direct light intensity by before applying.", new AcceptableValueRange<float>(0, 1)));
-            _moon_intensity_offset = Config.Bind("Moonlight", "Intensity Offset", 0.0f, "Amount to offset moon direct light intensity by before applying.");
-            _moon_shadow_multipier = Config.Bind("Moonlight", "Shadow Strength Multiplier", 1.0f, new ConfigDescription("Amount to multiply moon direct shadow strength by before applying.", new AcceptableValueRange<float>(0, 1)));
+            _sun_intensity_multipier = Config.Bind("Sunlight", "Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply sun direct light intensity by before applying.  Recommend 0.5", new AcceptableValueRange<float>(0, 1)));
+            _sun_intensity_offset = Config.Bind("Sunlight", "Intensity Offset", 0.0f, "Amount to offset sun direct light intensity by before applying.  Recommend 0.5");
+            _sun_shadow_multipier = Config.Bind("Sunlight", "Shadow Strength Multiplier", 1.0f, new ConfigDescription("Amount to multiply sun direct light shadow strength by before applying.  Recommend 0.8", new AcceptableValueRange<float>(0, 1)));
+            _moon_intensity_multipier = Config.Bind("Moonlight", "Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply moon direct light intensity by before applying.  Recommend 0.2", new AcceptableValueRange<float>(0, 1)));
+            _moon_intensity_offset = Config.Bind("Moonlight", "Intensity Offset", 0.0f, "Amount to offset moon direct light intensity by before applying.  Recommend 0.4");
+            _moon_shadow_multipier = Config.Bind("Moonlight", "Shadow Strength Multiplier", 1.0f, new ConfigDescription("Amount to multiply moon direct shadow strength by before applying.  Recommend 1.0", new AcceptableValueRange<float>(0, 1)));
             _ambient_mode = Config.Bind("Ambient Light", "Ambient Mode", AmbientMode.Trilight, "Ambient light mode to use.  WARNING: Skybox mode doesn't properly take into account the weather's effect on ambient light and isn't recommended");
             _ambient_day_intensity_multiplier = Config.Bind("Ambient Daylight", "Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply ambient light intensity by before applying.", new AcceptableValueRange<float>(0, 1)));
             _ambient_day_sky_intensity_multiplier = Config.Bind("Ambient Daylight", "Trilight Sky Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply Trilight ambient light intensity by before applying.", new AcceptableValueRange<float>(0, 1)));
             _ambient_day_equator_intensity_multiplier = Config.Bind("Ambient Daylight", "Trilight Equator Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply Trilight ambient light intensity by before applying.", new AcceptableValueRange<float>(0, 1)));
             _ambient_day_ground_intensity_multiplier = Config.Bind("Ambient Daylight", "Trilight Ground Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply Trilight ambient light intensity by before applying.", new AcceptableValueRange<float>(0, 1)));
-            _ambient_night_intensity_multiplier = Config.Bind("Ambient Nightlight", "Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply ambient light intensity by before applying.", new AcceptableValueRange<float>(0, 1)));
-            _ambient_night_sky_intensity_multiplier = Config.Bind("Ambient Nightlight", "Trilight Sky Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply Trilight ambient light intensity by before applying.", new AcceptableValueRange<float>(0, 1)));
-            _ambient_night_equator_intensity_multiplier = Config.Bind("Ambient Nightlight", "Trilight Equator Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply Trilight ambient light intensity by before applying.", new AcceptableValueRange<float>(0, 1)));
-            _ambient_night_ground_intensity_multiplier = Config.Bind("Ambient Nightlight", "Trilight Ground Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply Trilight ambient light intensity by before applying.", new AcceptableValueRange<float>(0, 1)));
-            _cloud1_intensity_multiplier = Config.Bind("Weather", "Light Clouds Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity when there are light clouds.", new AcceptableValueRange<float>(0, 1)));
-            _cloud2_intensity_multiplier = Config.Bind("Weather", "Partly Cloudy Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity when it is partly cloudy.", new AcceptableValueRange<float>(0, 1)));
-            _cloud3_intensity_multiplier = Config.Bind("Weather", "Mostly Cloudy Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity when it is mostly cloudy.", new AcceptableValueRange<float>(0, 1)));
-            _cloud4_intensity_multiplier = Config.Bind("Weather", "Completely Cloudy Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity when it is completely cloudy.", new AcceptableValueRange<float>(0, 1)));
-            _fog_intensity_multiplier = Config.Bind("Weather", "Foggy Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity when it is foggy.", new AcceptableValueRange<float>(0, 1)));
-            _rain_intensity_multiplier = Config.Bind("Weather", "Rain Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity when it is raining.", new AcceptableValueRange<float>(0, 1)));
-            _storm_intensity_multiplier = Config.Bind("Weather", "Storm Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity when it is heavily storming.", new AcceptableValueRange<float>(0, 1)));
-            _torchColorRed = Config.Bind("Objects", "Fire Light Color Red", 1.0f, new ConfigDescription("Amount to scale the Red color element of fires in the game", new AcceptableValueRange<float>(0, 2)));
-            _torchColorGreen = Config.Bind("Objects", "Fire Light Color Green", 1.0f, new ConfigDescription("Amount to scale the Green color element of fires in the game", new AcceptableValueRange<float>(0, 2)));
-            _torchColorBlue = Config.Bind("Objects", "Fire Light Color Blue", 1.0f, new ConfigDescription("Amount to scale the Blue color element of fires in the game", new AcceptableValueRange<float>(0, 2)));
-            _waistLampColor = Config.Bind("Objects", "Waist Lamp Color", new Color (1.0f, 0.6f, 0.2f, 1.0f), "Color Value to set waist lamps to");
-            _playerLampColor = Config.Bind("Objects", "Player Lamp Color", new Color(1.0f, 0.56f, 0.09f, 1.0f), "Color Value to set player lamps and torches to");
-            _waistLampShadows = Config.Bind("Objects", "Waist Lamp Shadows", LightShadows.None, "Should waist lamps cast shadows.  WARNING: can cause a performance drop");
-            _playerLampShadows = Config.Bind("Objects", "Player Lamp Shadows", LightShadows.None, "Should player lamps cast shadows.  WARNING: can cause a performance drop");
+            _ambient_night_intensity_multiplier = Config.Bind("Ambient Nightlight", "Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply ambient light intensity by before applying.  Recommend 0.4", new AcceptableValueRange<float>(0, 1)));
+            _ambient_night_sky_intensity_multiplier = Config.Bind("Ambient Nightlight", "Trilight Sky Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply Trilight ambient light intensity by before applying.  Recommend 0.4", new AcceptableValueRange<float>(0, 1)));
+            _ambient_night_equator_intensity_multiplier = Config.Bind("Ambient Nightlight", "Trilight Equator Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply Trilight ambient light intensity by before applying.  Recommend 0.2", new AcceptableValueRange<float>(0, 1)));
+            _ambient_night_ground_intensity_multiplier = Config.Bind("Ambient Nightlight", "Trilight Ground Intensity Multiplier", 1.0f, new ConfigDescription("Amount to multiply Trilight ambient light intensity by before applying.  Recommend 0.3", new AcceptableValueRange<float>(0, 1)));
+            _cloud1_intensity_multiplier = Config.Bind("Weather", "Light Clouds Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity and shadow strength when there are light clouds.  Recommend 0.95", new AcceptableValueRange<float>(0, 1)));
+            _cloud2_intensity_multiplier = Config.Bind("Weather", "Partly Cloudy Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity and shadow strength when it is partly cloudy.  Recommend 0.9", new AcceptableValueRange<float>(0, 1)));
+            _cloud3_intensity_multiplier = Config.Bind("Weather", "Mostly Cloudy Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity and shadow strength when it is mostly cloudy.  Recommend 0.85", new AcceptableValueRange<float>(0, 1)));
+            _cloud4_intensity_multiplier = Config.Bind("Weather", "Completely Cloudy Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity and shadow strength when it is completely cloudy.  Recommend 0.8", new AcceptableValueRange<float>(0, 1)));
+            _fog_intensity_multiplier = Config.Bind("Weather", "Foggy Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity and shadow strength when it is foggy.  Recommend 0.7", new AcceptableValueRange<float>(0, 1)));
+            _rain_intensity_multiplier = Config.Bind("Weather", "Rain Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity and shadow strength when it is raining.  Recommend 0.7", new AcceptableValueRange<float>(0, 1)));
+            _storm_intensity_multiplier = Config.Bind("Weather", "Storm Intensity Multiplier", 1.0f, new ConfigDescription("Amount to adjust sun/moon light intensity when it is heavily storming.  Recommend 0.5", new AcceptableValueRange<float>(0, 1)));
+            _torchColorRed = Config.Bind("Objects", "Fire Light Color Red", 1.0f, new ConfigDescription("Amount to scale the Red color element of fires in the game.  Recommend 0.8", new AcceptableValueRange<float>(0, 2)));
+            _torchColorGreen = Config.Bind("Objects", "Fire Light Color Green", 1.0f, new ConfigDescription("Amount to scale the Green color element of fires in the game.  Recommend 1.0", new AcceptableValueRange<float>(0, 2)));
+            _torchColorBlue = Config.Bind("Objects", "Fire Light Color Blue", 1.0f, new ConfigDescription("Amount to scale the Blue color element of fires in the game.  Recommend 1.0", new AcceptableValueRange<float>(0, 2)));
+            _waistLampColor = Config.Bind("Objects", "Waist Lamp Color", new Color (1.0f, 0.6f, 0.2f, 1.0f), "Color Value to set waist lamps to.  Recommend CC9933FF");
+            _playerLampColor = Config.Bind("Objects", "Player Lamp Color", new Color(1.0f, 0.56f, 0.09f, 1.0f), "Color Value to set player lamps and torches to.  Recomment CC8F17FF");
+            _waistLampShadows = Config.Bind("Objects", "Waist Lamp Shadows", LightShadows.None, "Should waist lamps cast shadows.  WARNING: can cause a performance drop.  Recommend Soft or None");
+            _playerLampShadows = Config.Bind("Objects", "Player Lamp Shadows", LightShadows.None, "Should player lamps cast shadows.  WARNING: can cause a performance drop.  Recommend Soft or None");
             _waistLampShadowStrength = Config.Bind("Objects", "Waist Lamp Shadow Strength", 1.0f, new ConfigDescription("Waist lamp shadow strength if shadows enabled.  Recommend 0.4", new AcceptableValueRange<float>(0, 1)));
-            _playerLampShadowStrength = Config.Bind("Objects", "Player Lamp Shadow Strength", 1.0f, new ConfigDescription("Player lamp shadow strength if shadows enabled.Recommend 0.4", new AcceptableValueRange<float>(0, 1)));
-            _waistLampShadowNearPlane = Config.Bind("Objects", "Waist Lamp Shadow Near Plane", 0.2f, "Near clip plane of waist lamp shadows, if enabled");
-            _playerLampShadowNearPlane = Config.Bind("Objects", "Player Lamp Shadow Near Plane", 0.2f, "Near clip plane of player lamp shadows, if enabled");
+            _playerLampShadowStrength = Config.Bind("Objects", "Player Lamp Shadow Strength", 1.0f, new ConfigDescription("Player lamp shadow strength if shadows enabled.  Recommend 0.4", new AcceptableValueRange<float>(0, 1)));
+            _waistLampShadowNearPlane = Config.Bind("Objects", "Waist Lamp Shadow Near Plane", 0.2f, "Near clip plane of waist lamp shadows, if enabled.  Recommend 1.5");
+            _playerLampShadowNearPlane = Config.Bind("Objects", "Player Lamp Shadow Near Plane", 0.2f, "Near clip plane of player lamp shadows, if enabled.  Recommend 0.4");
             _portableLampUpdateRate = Config.Bind("Objects", "Portable Lamp Update Rate", 180, new ConfigDescription("Controls how often to check for changes to portable lamps.", new AcceptableValueRange<int>(1, 1000)));
 
             harmony = new Harmony("AIEnvironmentalLighting");
@@ -139,20 +139,25 @@ namespace EnvironmentalLighting
             var currentWeather = Singleton<Manager.Game>.Instance?.Environment?.Weather;
             if (currentWeather != null)
             {
-                if (currentWeather == AIProject.Weather.Storm)
-                    directLightIntensity *= _storm_intensity_multiplier.Value;
-                else if (currentWeather == AIProject.Weather.Rain)
-                    directLightIntensity *= _rain_intensity_multiplier.Value;
-                else if (currentWeather == AIProject.Weather.Fog)
-                    directLightIntensity *= _fog_intensity_multiplier.Value;
-                else if (currentWeather == AIProject.Weather.Cloud4)
-                    directLightIntensity *= _cloud4_intensity_multiplier.Value;
-                else if (currentWeather == AIProject.Weather.Cloud3)
-                    directLightIntensity *= _cloud3_intensity_multiplier.Value;
+                float intensityMultiplier = 1.0f;
+
+                if (currentWeather == AIProject.Weather.Cloud1)
+                    intensityMultiplier = _cloud1_intensity_multiplier.Value;
                 else if (currentWeather == AIProject.Weather.Cloud2)
-                    directLightIntensity *= _cloud2_intensity_multiplier.Value;
-                else if (currentWeather == AIProject.Weather.Cloud1)
-                    directLightIntensity *= _cloud1_intensity_multiplier.Value;
+                    intensityMultiplier = _cloud2_intensity_multiplier.Value;
+                else if (currentWeather == AIProject.Weather.Cloud3)
+                    intensityMultiplier = _cloud3_intensity_multiplier.Value;
+                else if (currentWeather == AIProject.Weather.Cloud4)
+                    intensityMultiplier = _cloud4_intensity_multiplier.Value;
+                else if (currentWeather == AIProject.Weather.Fog)
+                    intensityMultiplier = _fog_intensity_multiplier.Value;
+                else if (currentWeather == AIProject.Weather.Rain)
+                    intensityMultiplier = _rain_intensity_multiplier.Value;
+                else if (currentWeather == AIProject.Weather.Storm)
+                    intensityMultiplier = _storm_intensity_multiplier.Value;
+
+                directLightIntensity *= intensityMultiplier;
+                directLightShadowStrength *= intensityMultiplier;
             }
 
             if (RenderSettings.ambientMode == AmbientMode.Trilight)
